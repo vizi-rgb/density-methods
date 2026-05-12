cead1e7dd143d5a42370d2d0c48096d417b25631
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2026-05-13 01:21:46 +0200

    feat: integrate HeatmapFactory and HeatmapVisualizer for heatmap generation

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from pathlib import Path
 
 import cv2
 
-from core.adapter.predictions import PredictionsAdapter
+from core.adapter.predictions_adapter import PredictionsAdapter
+from core.heatmap.heatmap_factory import HeatmapFactory
+from core.heatmap.heatmap_visualizer import HeatmapVisualizer
 from models.yolo.yolo import YOLOModel
 from project_root import PROJECT_ROOT
 
-path = "data/datasets/mall_dataset/frames/seq_000001.jpg"
-
+path = "data/datasets/mall_dataset/frames/"
 
 def _resolve_path(image_path: str) -> Path:
     path_obj = Path(image_path)
@@ -19,29 +20,17 @@ def _resolve_path(image_path: str) -> Path:
 def main() -> None:
     model = YOLOModel(path)
     raw_predictions = model.run_prediction(show=False)
-    points_by_prediction = PredictionsAdapter().to_points(raw_predictions)
+    predictions = PredictionsAdapter().to_predictions(raw_predictions)
 
-    if not points_by_prediction:
+    if not predictions:
         raise RuntimeError("No predictions were produced.")
 
-    points = points_by_prediction[0]
-    image_path = _resolve_path(path)
-    image = cv2.imread(str(image_path))
-    if image is None:
-        raise FileNotFoundError(f"Could not load image from path: {image_path}")
-
-    for x, y in points:
-        cv2.circle(
-            image,
-            (int(round(x)), int(round(y))),
-            4,
-            (0, 0, 255),
-            thickness=-1,
-        )
-
-    cv2.imshow("YOLO points", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    hf = HeatmapFactory(480, 640)
+    hv = HeatmapVisualizer(fixed_max=1000, alpha=0.5, sigma=15)
+    path_base = Path("out")
+    for num, prediction in enumerate(predictions):
+        heatmap = hf.get_heatmap_from_streamed_prediction(prediction)
+        hv.draw(heatmap, prediction.image_path, save_path= path_base / f"{num}.png")
 
 
 if __name__ == "__main__":

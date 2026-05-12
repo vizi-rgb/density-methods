6b5140b7aa33de76fcdb40a22f90d9847b2a614d
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2026-05-13 01:22:47 +0200

    feat: update HeatmapFactory to handle streamed predictions and batch predictions

--- core/heatmap/heatmap_factory.py
+++ core/heatmap/heatmap_factory.py
@@ -2,6 +2,8 @@ import math
 
 import numpy as np
 
+from core.adapter.predictions_adapter import Prediction
+
 class HeatmapFactory:
     def __init__(self, height, width):
         self.height = height
@@ -12,10 +14,19 @@ class HeatmapFactory:
         if self.heatmap is None:
             self.heatmap = np.zeros((self.height, self.width), dtype=np.float32)
 
-    def get_heatmap_from_prediction_points(self, prediction_points: list[list[tuple[float, float]]]):
+    def get_heatmap_from_streamed_prediction(self, prediction: Prediction):
         self.init_heatmap()
-        for points in prediction_points:
-            for x, y in points:
+        for x, y in prediction.points:
+            x = min(max(math.floor(x), 0), self.width - 1)
+            y = min(max(math.floor(y), 0), self.height - 1)
+            self.heatmap[y, x] += 1
+        print(f"Returning heatmap {prediction.image_path}")
+        return self.heatmap
+
+    def get_heatmap_from_predictions(self, predictions: list[Prediction]):
+        self.init_heatmap()
+        for prediction in predictions:
+            for x, y in prediction.points:
                 x = min(max(math.floor(x), 0), self.width - 1)
                 y = min(max(math.floor(y), 0), self.height - 1)
                 self.heatmap[y, x] += 1

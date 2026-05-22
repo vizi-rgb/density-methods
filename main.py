ee40fbb7a9bb7472aac3e345afdc95448741ed7d
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2026-05-22 22:11:32 +0200

    feat: update heatmap dimensions and adjust image path handling for new video

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from pathlib import Path
-
-import cv2
-
-from core.adapter.predictions_adapter import PredictionsAdapter, StreamedPredictionsAdapter
-from core.adapter.predictions_adapter_factory import PredictionsAdapterFactory, StreamedPredictionsAdapterFactory
+from core.adapter.predictions_adapter_factory import StreamedPredictionsAdapterFactory
 from core.heatmap.heatmap_factory import HeatmapFactory
 from core.heatmap.heatmap_visualizer import HeatmapVisualizer
 from models.yolo.yolo import YOLOModel
 from project_root import PROJECT_ROOT
 
-path = "data/datasets/mall_dataset/frames/"
+# path = "data/datasets/mall_dataset/frames/"
+path = "data/datasets/yt/walking_people.mp4"
 
 def _resolve_path(image_path: str) -> Path:
     path_obj = Path(image_path)
@@ -23,14 +20,14 @@ def main() -> None:
     raw_predictions = model.run_tracking(show=False, stream=True)
     predictions_adapter = StreamedPredictionsAdapterFactory.for_model(model)
 
-    hf = HeatmapFactory(480, 640)
-    hv = HeatmapVisualizer(fixed_max=100, alpha=0.5, sigma=10)
-    path_base = Path("out")
+    hf = HeatmapFactory(1080, 1920)
+    hv = HeatmapVisualizer(fixed_max=60, alpha=0.5, sigma=10)
+    path_base= Path("out")
 
     for num, prediction in enumerate(raw_predictions):
         processed_prediction = predictions_adapter.to_predictions(prediction)
         heatmap = hf.get_heatmap_from_streamed_prediction(processed_prediction)
-        hv.draw(heatmap["down"], processed_prediction.image_path, save_path= path_base / f"{num}.png")
+        hv.draw(heatmap["down"], processed_prediction.image, save_path= path_base / f"{num}.png")
 
 
 if __name__ == "__main__":

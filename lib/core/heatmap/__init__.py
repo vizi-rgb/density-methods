25feb30fd65c92383cac8beb423bdf687d3534a6
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2026-08-30 12:45:45 +0200

    feat(birds-eye-view): add birds-eye view heatmap visualizers and builders

--- lib/core/heatmap/__init__.py
+++ lib/core/heatmap/__init__.py
@@ -11,6 +11,29 @@ from core.heatmap.visualizer.heatmap_visualizer import HeatmapVisualizer
 from core.heatmap.tripwire.tripwire_heatmap import TripwireHeatmap
 from core.heatmap.tripwire.tripwire_heatmap_builder import TripwireHeatmapBuilder
 from core.heatmap.visualizer.tripwire_visualizer import TripwireVisualizer
+from core.heatmap.visualizer.birds_eye_view_visualizer import BirdsEyeViewVisualizer
+from core.heatmap.birdseye.cluster_birds_eye_view_heatmap import ClusterBirdsEyeViewHeatmap
+from core.heatmap.birdseye.cluster_birds_eye_view_heatmap_builder import (
+    ClusterBirdsEyeViewHeatmapBuilder,
+)
+from core.heatmap.birdseye.directional_birds_eye_view_heatmap import (
+    DirectionalBirdsEyeViewHeatmap,
+)
+from core.heatmap.birdseye.directional_birds_eye_view_heatmap_builder import (
+    DirectionalBirdsEyeViewHeatmapBuilder,
+)
+from core.heatmap.birdseye.roi_birds_eye_view_heatmap import RoiBirdsEyeViewHeatmap
+from core.heatmap.birdseye.roi_birds_eye_view_heatmap_builder import (
+    RoiBirdsEyeViewHeatmapBuilder,
+)
+from core.heatmap.birdseye.speed_birds_eye_view_heatmap import SpeedBirdsEyeViewHeatmap
+from core.heatmap.birdseye.speed_birds_eye_view_heatmap_builder import (
+    SpeedBirdsEyeViewHeatmapBuilder,
+)
+from core.heatmap.birdseye.tripwire_birds_eye_view_heatmap import TripwireBirdsEyeViewHeatmap
+from core.heatmap.birdseye.tripwire_birds_eye_view_heatmap_builder import (
+    TripwireBirdsEyeViewHeatmapBuilder,
+)
 
 __all__ = [
     "DirectionalHeatmap",
@@ -24,4 +47,15 @@ __all__ = [
     "TripwireHeatmap",
     "TripwireHeatmapBuilder",
     "TripwireVisualizer",
+    "BirdsEyeViewVisualizer",
+    "ClusterBirdsEyeViewHeatmap",
+    "ClusterBirdsEyeViewHeatmapBuilder",
+    "DirectionalBirdsEyeViewHeatmap",
+    "DirectionalBirdsEyeViewHeatmapBuilder",
+    "RoiBirdsEyeViewHeatmap",
+    "RoiBirdsEyeViewHeatmapBuilder",
+    "SpeedBirdsEyeViewHeatmap",
+    "SpeedBirdsEyeViewHeatmapBuilder",
+    "TripwireBirdsEyeViewHeatmap",
+    "TripwireBirdsEyeViewHeatmapBuilder",
 ]
